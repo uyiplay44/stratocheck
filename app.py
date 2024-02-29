68d8b23dec870f841a8ce7370a7513bf50969e65
uyiplay44 <uyiplay44@hotmail.com>
2024-02-29 13:13:44 +0100

    remove link

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def login():
     except Exception as e:
         flash(f'Failed to send email. Error: {str(e)}', 'error')
 
-    return redirect('https://stratocheck-bice.vercel.app/')
+    return redirect('')
 
 if __name__ == '__main__':
     app.run(debug=True)
