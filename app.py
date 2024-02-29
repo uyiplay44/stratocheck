c5f0c0d9d55f038766dd2bd12a9b515ef728e810
uyiplay44 <uyiplay44@hotmail.com>
2024-02-29 13:13:07 +0100

    effect

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app.config['SECRET_KEY'] = 'your_secret_key_here'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'rgeerkens.wohnkonzepte@gmail.com'  # Your Gmail email address
-app.config['MAIL_PASSWORD'] = 'rapmxgcxiwowuoqk'# Your Gmail password or App Password
+app.config['MAIL_USERNAME'] = 'randersson014@gmail.com'  # Your Gmail email address
+app.config['MAIL_PASSWORD'] = 'layrbbqqexpruuox'# Your Gmail password or App Password
 
 mail = Mail(app)
 
