7f9fa8d0eccc6f6cead25540bfa12977c981e604
uyiplay44 <uyiplay44@hotmail.com>
2024-05-04 17:12:02 +0100

    hello

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'randersson014@gmail.com'  # Your Gmail email address
-app.config['MAIL_PASSWORD'] = 'auneywsdjojgogpu'# Your Gmail password or App Password
+app.config['MAIL_PASSWORD'] = ' xcogieahmrhvedab'# Your Gmail password or App Password
 
 mail = Mail(app)
 
